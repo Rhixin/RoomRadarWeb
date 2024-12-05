b12dfa5f4c493ff19f58fd5948df37f23d59d0a8
Rhixin <vallestedted@gmail.com>
2024-12-06 00:08:48 +0800

    Added google auth

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-// see here https://next-auth.js.org/getting-started/example
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {

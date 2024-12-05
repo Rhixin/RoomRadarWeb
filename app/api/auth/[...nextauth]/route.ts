cf4fa1e6bad2665fc87ba9523f223d620731627e
Rhixin <vallestedted@gmail.com>
2024-12-05 22:34:34 +0800

    Added google auth [fix bug first]

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!profile?.email) {
+        throw new Error("No profile");
+      }
+
+      return true;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
